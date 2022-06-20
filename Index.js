26832ed56075c276df48086657099b17f8e99a19
opramo <haepram@gmail.com>
2022-06-20 10:23:12 +0700

    worrd variable

--- Index.js
+++ Index.js
@@ -1,4 +1,5 @@
-let word = 'Hello, Purwadhika!'
-let word2 = 'Hello, World!'
+let word = "Hello, Purwadhika!";
+let word2 = "Hello, World!";
+let worrd = "Hello Hello";
 
-console.log(word)
+console.log(word);
